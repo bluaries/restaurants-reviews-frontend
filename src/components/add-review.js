6470db082b575abb9ca8edd545ffe8d72d5df0d7
arisa.pan <arisa.pan@mfec.co.th>
2022-01-30 10:24:37 +0700

    Basic frontend

---
+++ src/components/add-review.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function AddReview() {
+  return (
+    <div className="App">
+      HELLO
+      
+    </div>
+  );
+}
+
+export default AddReview;
---
+++ src/components/login.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function RestaurantsList() {
+  return (
+    <div className="App">
+      HELLO
+      
+    </div>
+  );
+}
+
+export default RestaurantsList;
